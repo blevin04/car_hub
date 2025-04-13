2378f95c69a777df250563637a992a2e6566187c
blevin04 <jamestonito04@gmail.com>
2025-04-13 14:15:42 +0300

    more

--- lib/gamePages/revMatch.dart
+++ lib/gamePages/revMatch.dart
@@ -295,7 +295,6 @@ class _RevmatchState extends State<Revmatch> {
                     shrinkWrap: true,
                     physics:const  NeverScrollableScrollPhysics(),
                     itemBuilder: (BuildContext context, int index) {
-                      
                       return Card(
                         child: Stack(
                           alignment: Alignment.bottomLeft,
